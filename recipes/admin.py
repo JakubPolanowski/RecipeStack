73df92cd7454c3d2e9d8975606be8cd45032619a
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-23 18:18:11 -0500

    implemented basic admins for most models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-import models
+from . import models
 # Register your models here.
 
 
@@ -7,4 +7,66 @@ class FlavorInline(admin.StackedInline):
     model = models.Flavor
 
 
-admin.site.register(models.Flavor, FlavorInline)
+class FlavorAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name']
+
+
+admin.site.register(models.Flavor, FlavorAdmin)
+
+
+class MealAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name']
+
+
+admin.site.register(models.Meal, MealAdmin)
+
+
+class UnitAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name']
+
+
+admin.site.register(models.Unit, UnitAdmin)
+
+
+class FoodGroupMembershipInline(admin.TabularInline):
+    model = models.FoodGroup.ingredients.through
+
+
+class IngredientAdmin(admin.ModelAdmin):  # TODO make more usable
+    search_fields = ['name']
+    list_display = ['name']
+    inlines = [FoodGroupMembershipInline]
+
+
+admin.site.register(models.Ingredient, IngredientAdmin)
+
+
+class FoodGroupAdmin(admin.ModelAdmin):  # TODO make more usable
+    search_fields = ['name']
+    list_display = ['name']
+    inlines = [FoodGroupMembershipInline]
+    exclude = ['members']
+
+
+admin.site.register(models.FoodGroup, FoodGroupAdmin)
+
+
+class DietAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name']
+
+
+admin.site.register(models.Diet, DietAdmin)
+
+
+class RecipeAdmin(admin.ModelAdmin):  # TODO make more usable
+    search_fields = ['title']
+    list_display = ['title']
+
+
+admin.site.register(models.Recipe, RecipeAdmin)
+
+# TODO added recipeingredients admin
