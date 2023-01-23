ffdca4e601a7cab86fc0143aa827d052e462605f
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-22 23:51:43 -0500

    registered flavor

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+import models
 # Register your models here.
+
+
+class FlavorInline(admin.StackedInline):
+    model = models.Flavor
+
+
+admin.site.register(models.Flavor, FlavorInline)
