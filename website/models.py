5aa01117956b498a7c66d737b8ce9a12214b5a2e
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-22 23:31:32 -0500

    implemented simple models

--- website/models.py
+++ website/models.py
@@ -2,6 +2,33 @@ from django.db import models
 
 # Create your models here.
 
+
+class Flavor(models.Model):
+    # various flavors that are attached to recipes many to many
+    name = models.CharField(max_length=50)
+
+
+class Meal(models.Model):
+    # various meals (such as morning, Dinner) that are attached to recipes many to many
+    name = models.CharField(max_length=50)
+
+
+class Ingredient(models.Model):
+    # ingredient used in recipe
+    name = models.CharField(max_length=50)
+
+
+class FoodGroups(models.Model):
+    # food groups that ingredients belong in
+    name = models.CharField(max_length=50)
+    ingredients = models.ManyToManyField(Ingredient)
+
+
+class Diets(models.Model):
+    # diet that are attached to recipes many to many
+    name = models.CharField(max_length=50)
+
+
 # TODO Recipe
 # title
 # content
@@ -9,19 +36,3 @@ from django.db import models
 # meal
 # ingredients
 # diets
-
-# TODO flavor
-# name
-
-# TODO meal
-# name
-
-# TODO ingredient
-# name
-
-# TODO food groups
-# name
-# ingredients
-
-# TODO diets
-# name
