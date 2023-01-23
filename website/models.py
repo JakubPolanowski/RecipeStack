69cda76a3ec918696072263110f5307d82d6e797
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-22 23:20:24 -0500

    added todos

--- website/models.py
+++ website/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+# TODO Recipe
+# title
+# content
+# flavor
+# meal
+# ingredients
+# diets
+
+# TODO flavor
+# name
+
+# TODO meal
+# name
+
+# TODO ingredient
+# name
+
+# TODO food groups
+# name
+# ingredients
+
+# TODO diets
+# name
