f346c69e8f3d2c043d2e8dff8d3913aee7a7f43a
Jakub Polanowski <jakubpol27@gmail.com>
2023-01-22 23:45:31 -0500

    implement unit and recipe models

--- website/models.py
+++ website/models.py
@@ -13,6 +13,11 @@ class Meal(models.Model):
     name = models.CharField(max_length=50)
 
 
+class Unit(models.Model):  # TODO figure out how to handle compatible units/conversion
+    # volume, temperature, etc. units
+    name = models.CharField(max_length=10)
+
+
 class Ingredient(models.Model):
     # ingredient used in recipe
     name = models.CharField(max_length=50)
@@ -24,15 +29,22 @@ class FoodGroups(models.Model):
     ingredients = models.ManyToManyField(Ingredient)
 
 
-class Diets(models.Model):
+class Diet(models.Model):
     # diet that are attached to recipes many to many
     name = models.CharField(max_length=50)
 
 
-# TODO Recipe
-# title
-# content
-# flavor
-# meal
-# ingredients
-# diets
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    flavors = models.ManyToManyField(Flavor)
+    meals = models.ManyToManyField(Meal)
+    diets = models.ManyToManyField(Diet)
+    # TODO figure out how to handle steps/instructions
+
+
+class RecipeIngredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    unit = models.ForeignKey(Unit, on_delete=models.PROTECT)
+    amount = models.FloatField()
